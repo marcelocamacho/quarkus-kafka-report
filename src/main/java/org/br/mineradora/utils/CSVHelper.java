c08cff81d0631313e37ca23f6302cc75b09f8c95
Marcelo S. Camacho <marcelocamacho.ufpa@gmail.com>
2024-08-02 23:14:16 -0300

    Cria o arquivo que irá serializar e gerar uma planilha com os dados necessários

---
+++ src/main/java/org/br/mineradora/utils/CSVHelper.java
@@ -0,0 +1,31 @@
+package org.br.mineradora.utils;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class CSVHelper {
+    public static ByteArrayInputStream OpportunitiesToCSV(List<OpportunityDTO> opportunities){
+        final CSVFormat format = CSVFormat.DEFAULT.withHeader("ID Proposta", "Cliente", "Preço por Tonelada", "Melhor cotação da moeda");
+
+        try(
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            CSVPrinter csvPrinter = new CSVPrinter(new PrintWriter(out), format)){
+
+                for (OpportunityDTO opps: opportunities){
+                    List<String> data = Arrays.asList(String.valueOf(opps.getProposalId()),
+                        opps.getCustomer(), String.valueOf(opps.getPriceTonnes),
+                        String.valueOf(opps.getLastDollarQuotation()));
+                        csvPrinter.printRecord(data);
+                }
+                csvPrinter.flush();
+                return new ByteArrayInputStream(out.toByteArray());
+            } catch( IOException e){
+                throw new RuntimeException("Fail to import data to CSV file: "+ e.getMessage());
+            }
+        
+
+    }
+}
