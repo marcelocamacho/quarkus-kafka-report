db98b7202468c6a70d7c755acf355f580f705bee
Marcelo S. Camacho <marcelocamacho.ufpa@gmail.com>
2024-08-02 23:54:28 -0300

    Classe utilitária que fará a escrita da planilha

--- src/main/java/org/br/mineradora/utils/CSVHelper.java
+++ src/main/java/org/br/mineradora/utils/CSVHelper.java
@@ -3,12 +3,21 @@ package org.br.mineradora.utils;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.List;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+import org.br.mineradora.dto.OpportunityDTO;
+
 public class CSVHelper {
     public static ByteArrayInputStream OpportunitiesToCSV(List<OpportunityDTO> opportunities){
-        final CSVFormat format = CSVFormat.DEFAULT.withHeader("ID Proposta", "Cliente", "Preço por Tonelada", "Melhor cotação da moeda");
+
+
+        @SuppressWarnings("deprecation")
+        final CSVFormat format = CSVFormat.DEFAULT
+        .withHeader("ID Proposta", "Cliente", "Preço por Tonelada", "Melhor cotação da moeda");
 
         try(
             ByteArrayOutputStream out = new ByteArrayOutputStream();
@@ -16,12 +25,13 @@ public class CSVHelper {
 
                 for (OpportunityDTO opps: opportunities){
                     List<String> data = Arrays.asList(String.valueOf(opps.getProposalId()),
-                        opps.getCustomer(), String.valueOf(opps.getPriceTonnes),
+                        opps.getCustomer(), String.valueOf(opps.getPriceTonne()),
                         String.valueOf(opps.getLastDollarQuotation()));
                         csvPrinter.printRecord(data);
                 }
                 csvPrinter.flush();
                 return new ByteArrayInputStream(out.toByteArray());
+                
             } catch( IOException e){
                 throw new RuntimeException("Fail to import data to CSV file: "+ e.getMessage());
             }
